2e83bbd67821b40ebf9bcb4d3f1f4fc837310f3d
Anointing-Olabode <anointingolabode@gmail.com>
2025-01-24 03:44:17 -0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 const SECRET_KEY = process.env.JWT_SECRET;
 
 // Middleware to enable Cross-Origin Resource Sharing for all routes
-app.use(cors());
+app.use(cors({ origin: 'https://authentication-app-4xfs.onrender.com'}));
 
 // Middleware to parse incoming JSON request bodies
 app.use(express.json());
